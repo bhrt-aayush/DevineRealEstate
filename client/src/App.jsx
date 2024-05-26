8ab6fba8fb21c80106f1a68e04e1ce658a2d2392
xela-ale <bhrt.aayush@gmail.com>
2024-05-26 14:45:50 +0545

    Pages And Some Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
 export default function App() {
-  return (
-    <div>App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/SignIn" element={<SignIn/>}/>
+    <Route path="/SignUp" element={<SignUp/>}/>
+    <Route path="/Profile" element={<Profile/>}/>
+    <Route path="/About" element={<About/>}/>
+    
+  </Routes>
+  </BrowserRouter>
 }
